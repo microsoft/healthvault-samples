69897f1600b87be27663e307fd3d27c1aa4e9772
Justin Hutchings <justhu@microsoft.com>
2017-02-25 01:09:08 +0000

    Merged PR 6198: Fixed documentation for PS script. Changed parameter from string to GUID
    Fixed documentation for PS script, fixed bug about support for EU instance

--- dotNET/HealthVaultProviderManagementPortal/Update-WebConfig.ps1
+++ dotNET/HealthVaultProviderManagementPortal/Update-WebConfig.ps1
@@ -4,34 +4,56 @@
 This script can be used to quickly edit the Web.config file for your sample. 
 
 .EXAMPLE
-./Update-WebConfig.ps1 -ApplicationID "00000000-0000-0000-0000-000000000000" -Region US
+./Update-WebConfig.ps1 -ApplicationId "00000000-0000-0000-0000-000000000000" -Instance US
 
 .PARAMETER Reset
 The Reset parameter restores this file back to the state which is required by GitHub to protect your app's ApplicationId.
 
-.PARAMETER ApplicationID
+.PARAMETER ApplicationId
 The ApplicationId parameter allows you to specify the GUID value (without braces) which is registed in HealthVault for your app.
+
+.PARAMETER Instance
+The Instance parameter allows you to specify the appropriate server instances for HealthVault.
 
 #>
 
 Param
 (
-[switch] $Reset,
-[string] $ApplicationID
+    [switch] $Reset,
+
+    [guid] $ApplicationId,
+
+    [ValidateSet('US', 'EU', IgnoreCase = $true)]
+    [string] $Instance
 )
 
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
 $webConfigPath = $PSScriptRoot + "\HealthVaultProviderManagementPortal\Web.config"
-$webConfigXml = (select-xml -Path $webConfigPath -XPath "/").Node
+[xml] $webConfigXml = Get-Content -Path $webConfigPath -Raw
 $defaultApplicationId = "your application ID here";
 
-if ($Reset.IsPresent) {
-    ($webConfigXml | Select-Xml -XPath "//add[@key='ApplicationId']").Node.Attributes.GetNamedItem("value").InnerText = $defaultApplicationId;
-    $webConfigXml.Save($webConfigPath)
+if ($Reset) {
+    ($webConfigXml | Select-Xml -XPath "//add[@key='ApplicationId']/@value").Node.InnerText = $defaultApplicationId;
+}
+elseif ($ApplicationId) {
+    ($webConfigXml | Select-Xml -XPath "//add[@key='ApplicationId']/@value").Node.InnerText = $ApplicationId
 }
 
-if ($ApplicationID -ne $null -and $ApplicationID -ne "") {
-    ($webConfigXml | Select-Xml -XPath "//add[@key='ApplicationId']").Node.Attributes.GetNamedItem("value").InnerText = $ApplicationID
-    $webConfigXml.Save($webConfigPath)
+switch ($Instance) {
+    
+    'US' {
+        ($webConfigXml | Select-xml -XPath "//add[@key='ShellUrl']/@value").Node.InnerText = "https://account.healthvault-ppe.com/"
+        ($webConfigXml | Select-xml -XPath "//add[@key='HealthServiceUrl']/@value").Node.InnerText = "https://platform.healthvault-ppe.com/platform/"
+        ($webConfigXml | Select-xml -XPath "//add[@key='RestHealthServiceUrl']/@value").Node.InnerText = "https://data.ppe.microsofthealth.net"
+    }
+
+    'EU' {
+        ($webConfigXml | Select-xml -XPath "//add[@key='ShellUrl']/@value").Node.InnerText = "https://account.healthvault-ppe.co.uk/"
+        ($webConfigXml | Select-xml -XPath "//add[@key='HealthServiceUrl']/@value").Node.InnerText = "https://platform.healthvault-ppe.co.uk/platform/"
+        ($webConfigXml | Select-xml -XPath "//add[@key='RestHealthServiceUrl']/@value").Node.InnerText = "https://data.ppe.microsoft.health.co.uk"
+    }
 }
 
-
+$webConfigXml.Save($webConfigPath)
